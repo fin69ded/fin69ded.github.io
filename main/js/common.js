2368e583fa884e6a5585f3ad17ccb8dc19a9b59c
st69ray <st69ray@gmail.com>
2021-10-04 16:38:03 +0300

    Change style of the page v.0.2

--- main/js/common.js
+++ main/js/common.js
@@ -1,6 +1,10 @@
-$(document).ready(function() {
-	$('#fullpage').fullpage({
-		navigation: true,
-		navigationPosition: 'right'
-	});
+["load", "resize"].forEach((el) => {
+  window.addEventListener(el, () => {
+    const secondScreen = document.querySelectorAll(".second-screen");
+    const works = document.querySelectorAll(".works");
+
+    for (let i = 0; i < secondScreen.length; i++) {
+      secondScreen[i].style.height = `${works[i].clientHeight + 236}px`;
+    }
+  });
 });
